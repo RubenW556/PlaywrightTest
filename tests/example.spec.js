4cd25d5447cf9b228ed2e485e63beda96149cc3b
ruben.wijnbergen <ruben.wijnbergen@novadoc.nl>
2024-03-14 14:39:22 +0100

    Commit for testing

--- tests/example.spec.js
+++ tests/example.spec.js
@@ -4,19 +4,20 @@ const { test, expect } = require('@playwright/test');
 
 test.describe('New Todo', () => {
 
-test('has title', async ({ page }) => {
-  await page.goto('https://playwright.dev/');
+  test('has title', async ({ page }) => {
+    await page.goto('http://win-1ics5jffm16.dev.local:9080/navigator/');
 
-  // Expect a title "to contain" a substring.
-  await expect(page).toHaveTitle(/Playwright/);
-});
+    // Expect a title "to contain" a substring.
+    await expect(page).toHaveTitle(/Content/);
+  });
 
-test('get started link', async ({ page }) => {
-  await page.goto('https://playwright.dev/');
-
-  // Click the get started link.
-  await page.getByRole('link', { name: 'Get started' }).click();
-
-  // Expects page to have a heading with the name of Installation.
-  await expect(page.getByRole('heading', { name: 'Installation' })).toBeVisible();
-});})
+// test('get started link', async ({ page }) => {
+//   await page.goto('http://win-1ics5jffm16.dev.local:9080/navigator/');
+//
+//   // Click the get started link.
+//   await page.getByRole('link', { name: 'Get started' }).click();
+//
+//   // Expects page to have a heading with the name of Installation.
+//   await expect(page.getByRole('heading', { name: 'Installation' })).toBeVisible();
+// });
+})
